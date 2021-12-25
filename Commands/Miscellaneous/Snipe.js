ab2f9b3e051478d5a0e42d99990594f096c8835f
ThePywon <79721537+ThePywon@users.noreply.github.com>
2021-12-25 18:44:38 -0500

    Update Snipe.js

--- Commands/Miscellaneous/Snipe.js
+++ Commands/Miscellaneous/Snipe.js
@@ -1,4 +1,5 @@
 const { CommandInteraction, MessageEmbed } = require('discord.js');
+const Mongoose = require("mongodb");
 
 module.exports = {
     name: "snipe",
@@ -10,8 +11,28 @@ module.exports = {
      */
 
     async execute(interaction) {
-        const { channel } = interaction;
-
-        const Messages = await channel.messages.fetch();
+        if(Mongoose.connection.readyState == 1)
+        {
+            Guild.findOne({id: interaction.guild.id}).then(guild=>{
+                if(guild === null)
+                {
+                    interaction.reply({embeds: [
+                        new MessageEmbed().addField("No messages yet", "Be the first to send one!").setColor("#ff0000")
+                    ], ephemeral:true});
+                    return;
+                }
+                
+                interaction.reply({embeds: [
+                    new MessageEmbed().setAuthor(guild.latestMessage.user.name, guild.latestMessage.user.pfp)
+                    .setDescription(`${guild.latestMessage.content}\n\n*Sent in <#${guild.latestMessage.channel}> at <t:${guild.latestMessage.timestamp}:>*`).setColor("#ff0000")
+                ]});
+            });
+        }
+        else
+        {
+            interaction.reply({embeds: [
+                new MessageEmbed().addField("Database connection error!", "Tell the devs right now!").setColor("#ff0000")
+            ], ephemeral:true});
+        }
     }
 }
