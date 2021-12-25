325560de14530965e054f6289e5b8dc948a2ac81
ThePywon <79721537+ThePywon@users.noreply.github.com>
2021-12-25 17:33:24 -0500

    Update GuildDB.js

--- Structures/Schemes/GuildDB.js
+++ Structures/Schemes/GuildDB.js
@@ -1,2 +1,21 @@
 const Mongoose = require("mongodb");
 
+const Guild = Mongoose.model("Guild", new Mongoose.Schema({
+  id: {
+    type: ID,
+    required: [true,"Cannot create an instance of 'Guild' model without an id property."],
+    unique: true
+  },
+  latestMessage: {
+    user:{
+      id: {
+        type: ID,
+        unique: true
+      },
+      pfp: String
+    },
+    channel: ID,
+    content: String,
+    timestamp: Integer
+  }
+}));
