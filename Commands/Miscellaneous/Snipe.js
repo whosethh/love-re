22ea364561023fe8182585311a134bc9a891ac6c
ThePywon <79721537+ThePywon@users.noreply.github.com>
2021-12-25 16:59:33 -0500

    Update Snipe.js

--- Commands/Miscellaneous/Snipe.js
+++ Commands/Miscellaneous/Snipe.js
@@ -1,1 +1,17 @@
+const { CommandInteraction, MessageEmbed } = require('discord.js');
 
+module.exports = {
+    name: "snipe",
+    description: "Display the latest message sent in the server (bots excluded)",
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * 
+     */
+
+    async execute(interaction) {
+        const { channel } = interaction;
+
+        const Messages = await channel.messages.fetch();
+    }
+}
