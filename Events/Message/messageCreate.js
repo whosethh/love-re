986620a4e89abbfded4ebf1e45ad9d956acc10ec
ThePywon <79721537+ThePywon@users.noreply.github.com>
2021-12-25 17:58:47 -0500

    Update messageCreate.js

--- Events/Message/messageCreate.js
+++ Events/Message/messageCreate.js
@@ -1,4 +1,5 @@
 const { MessageEmbed, Message } = require('discord.js');
+const Mongoose = require('mongoose');
 
 module.exports = {
     name: "messageCreate",
@@ -10,8 +11,33 @@ module.exports = {
 
     execute(message) {
         if(message.author.bot) return;
-
         
+        var msgData = {
+            user:{
+                name: message.author.tag,
+                pfp: message.author.displayAvatarURL
+            },
+            channel: message.channel.name,
+            content: message.content,
+            timestamp: Date.now()
+        }
+
+        if(Mongoose.connection.readyState == 1)
+            Guild.findOne({id: message.guild.id}).then(guild=>{
+                if(server === null)
+                {
+                    server = new Server({
+                        id: message.guild.id,
+                        latestMessage:msgData
+                    });
+                    server.save();
+                }
+                else
+                {
+                    server.latestMessage = msgData;
+                    server.save();
+                }
+            });
     }
 
 }
