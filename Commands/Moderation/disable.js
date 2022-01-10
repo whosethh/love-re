b04a1715156a1db4ca8d95e28d637b40f671cc51
ThePywon <79721537+ThePywon@users.noreply.github.com>
2022-01-10 14:11:48 -0500

    Create disable.js

---
+++ Commands/Moderation/disable.js
@@ -0,0 +1,33 @@
+const { CommandInteraction, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "disable",
+    description: "Disable a slash command in this server.",
+    permission: "ADMIN",
+    options: [
+        {
+            name: "name",
+            description: "Please provide the slash command you want to disable.",
+            type: "STRING",
+            required: true
+        }
+    ],
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * 
+     */
+
+    async execute(interaction) {
+        const { channel, options } = interaction;
+
+        const Name = options.getString('name');
+
+        const Messages = await channel.messages.fetch();
+
+        const Response = new MessageEmbed()
+        .setColor('DARK_BUT_NOT_BLACK');
+      
+        
+    }
+}
